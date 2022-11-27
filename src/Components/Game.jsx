776ef84d7fdcafb250118e02d92f35c2ea1ca533
yunusemreakkas16 <Y.Emre_Akkas@outlook.com>
2022-11-27 22:02:35 +0300

    Update Game.jsx

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -3,11 +3,76 @@ import '../Assets/Game.css';
 import {Route, Routes} from "react-router-dom";
 
 function Game(){
+
+
+    const  Rock       = 0;
+    const  Scissor    = 1;
+    const  Paper      = 2;
+
+    let    Pcount     = 0;
+    let    Bcount     = 0;
+    let    flag       = true;
+    let    PlayerCard;
+    let    BotCard;
+    let    result ="";
+
+    //Default single player design.
+    console.log("fgfgfg")
+
+
+    for(;flag===true;) {
+        PlayerCard = Math.random(0,2);
+        BotCard    = Math.random(0,2);
+
+        if (PlayerCard === 0 && BotCard === 1) {
+            result="Congrats, you've won this round.";
+            Pcount++;
+        }
+        if (PlayerCard === 1 && BotCard === 0) {
+            result="You've lost this round.";
+            Bcount++;
+        }
+
+        if (PlayerCard === 0 && BotCard === 2) {
+            result="You've lost this round.";
+            Bcount++;
+        }
+
+        if (PlayerCard === 2 && BotCard === 0) {
+            result="Congrats, you've won this round.";
+            Pcount++;
+        }
+        if (PlayerCard === 1 && BotCard === 2) {
+            result="Congrats, you've won this round.";
+            Pcount++;
+        }
+
+        if (PlayerCard === 2 && BotCard === 1) {
+            result="You've lost this round.";
+            Bcount++;
+        }
+        if(Bcount===3 || Pcount===3)
+        {
+            flag =false;
+        }
+    }
+
+    if(Pcount===3)
+    {
+        result="Congrats you've won the game.";
+    }
+    else
+    {
+        result="You've lose the game.";
+    }
+
     return(
         <div>
-            <h1 className="yazi" id="bir">SinglePlayer</h1>
-
+            <h1 className="word">SinglePlayer</h1>
+            <h1>{result}</h1>
         </div>
 )
+
 }
+
 export default Game;
